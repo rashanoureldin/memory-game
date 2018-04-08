727f56cbf21b4efc73e0bc7d67e3361c72e346e3
rashanoureldin <rnoureldin2008@gmail.com>
2018-04-08 19:35:43 +0200

    Rating done

--- js/app.js
+++ js/app.js
@@ -9,7 +9,8 @@
  let moves = 0;
  let moveCounter = document.querySelector(".moves");
  let matchedCard = document.getElementsByClassName("match");
- let stars = document.querySelectorAll(".fa-star");
+//  const stars = document.querySelectorAll(".fa-star");
+// let star = [...stars];
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -22,17 +23,17 @@
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
 
-    var currentIndex = array.length, temporaryValue, randomIndex;
+	var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	while (currentIndex !== 0) {
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
 
-    return array;
+	return array;
 }
 
 
@@ -48,100 +49,103 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
-function startGame (){
+ function startGame (){
+ 	openedCards = [];
+ 	cards = shuffle(cards);
 
-	cards = shuffle(cards);
+ 	for (let i = 0 ;i< cards.length; i++) {
 
-	for (let i = 0 ;i< cards.length; i++) {
-
-	 	deck.innerHTML = "";
-	 	
-	 	
-	 [].forEach.call(cards, function(item) {
-
-	            deck.appendChild(item);
-	        });
-
-	  cards[i].classList.remove("show", "open", "match","disabled");
-	 };
+ 		deck.innerHTML = "";
 
 
-}
+ 		[].forEach.call(cards, function(item) {
 
-var openCard = function(){
+ 			deck.appendChild(item);
+ 		});
 
-		this.classList.toggle("open");
-		this.classList.toggle("show");
+ 		cards[i].classList.remove("show", "open", "match","disabled");
+ 	};
+
+
+ }
+
+ var openCard = function(){
+
+ 	this.classList.toggle("open");
+ 	this.classList.toggle("show");
 		// this.classList.toggle("disabled");
-}
-
-
-for (var i = 0; i < cards.length; i++){
-
-   card = cards[i];
-   card.addEventListener("click", openCard);
-   card.addEventListener("click", openCards);
-
-}
-
-function openCards(){
-	openedCards.push(this);
-	let cardLength = openedCards.length;
-	if(cardLength === 2){
-		movesCounter();
-		if(openedCards[0].innerHTML === openedCards[1].innerHTML){
-			matched();
-		}else{
-			unmatched();
-		}
 	}
-	
 
-}
 
-function matched(){
-	openedCards[0].classList.add('match','disabled');
-	openedCards[1].classList.add('match','disabled');
-	openedCards[0].classList.remove("show", "open");
-    openedCards[1].classList.remove("show", "open");
-    openedCards = [];
-}
-function unmatched(){
-	disabled();
-	setTimeout(function() {
+	for (var i = 0; i < cards.length; i++){
+
+		card = cards[i];
+		card.addEventListener("click", openCard);
+		card.addEventListener("click", openCards);
+
+	}
+
+	function openCards(){
+		openedCards.push(this);
+		let cardLength = openedCards.length;
+		if (cardLength === 1) {
+			movesCounter();
+		}
+		else if(cardLength === 2){
+			
+			if(openedCards[0].innerHTML === openedCards[1].innerHTML){
+				matched();
+			}else{
+				unmatched();
+			}
+		}
+
+
+	}
+
+	function matched(){
+		openedCards[0].classList.add('match','disabled');
+		openedCards[1].classList.add('match','disabled');
 		openedCards[0].classList.remove("show", "open");
 		openedCards[1].classList.remove("show", "open");
-		enable();
 		openedCards = [];
-	}, 800);
+	}
+	function unmatched(){
+		disabled();
+		setTimeout(function() {
+			openedCards[0].classList.remove("show", "open");
+			openedCards[1].classList.remove("show", "open");
+			enable();
+			openedCards = [];
+		}, 800);
 
-}
-function disabled(){
-	for(var i = 0; i < cards.length; i++){
-            card.classList.add('disabled');
-        }
-	
-}
-function enable(){
-	for(var i = 0; i < cards.length; i++){
-            card.classList.remove('disabled');
-        }
- 
-        for(var i = 0; i < matchedCard.length; i++){
-            matchedCard[i].classList.add("disabled");
-        }
-}
-function movesCounter(){
-	moves++;
-	moveCounter.innerHTML = moves;
-	if(moves == 1){
-        second = 0;
-        minute = 0; 
-        hour = 0;
-        startTimer();
-    }
-    rating();
-}	
+	}
+	function disabled(){
+		for(var i = 0; i < cards.length; i++){
+			card.classList.add('disabled');
+		}
+
+	}
+	function enable(){
+		for(var i = 0; i < cards.length; i++){
+			card.classList.remove('disabled');
+		}
+
+		for(var i = 0; i < matchedCard.length; i++){
+			matchedCard[i].classList.add("disabled");
+		}
+	}
+	function movesCounter(){
+		moves++;
+		moveCounter.innerHTML = moves;
+		if(moves == 1){
+			second = 0;
+			minute = 0; 
+			hour = 0;
+			startTimer();
+		}
+		rating();
+	}	
 // moveCounter[0].innerHTML = moves
 // let moveCounter = document.getElementsByClassName("moves")
 
@@ -149,30 +153,34 @@ var second = 0, minute = 0; hour = 0;
 var timer = document.querySelector(".timer");
 var interval;
 function startTimer(){
-    interval = setInterval(function(){
-        timer.innerHTML = minute+"mins "+second+"secs";
-        second++;
-        if(second == 60){
-            minute++;
-            second=0;
-        }
-        if(minute == 60){
-            hour++;
-            minute = 0;
-        }
-    },1000);
+	interval = setInterval(function(){
+		timer.innerHTML = minute+"mins "+second+"secs";
+		second++;
+		if(second == 59){
+			minute++;
+			second=0;
+		}
+		if(minute == 59){
+			hour++;
+			minute = 0;
+		}
+	},1000);
 }
+let stars = document.querySelectorAll(".fa-star");
 
 function rating(){
-	
-	stars.classList.remove("fa fa-star");
+
+
 	if (moves > 8 && moves < 12){
-		for(let i = 0; i<= stars.length; i++){
-			stars.classList.add("fa fa-star-o");
-		}
+		
+		for (var i = 0; i < 5 ; i++) {
+			if (i >3){
+				stars[i].classList.remove('fa-star'); 
+			}
+			
+		}		
+
 	}else if(moves > 12){
-		for(let i = 0; i<= stars.length; i++){
-			stars.classList.add("fa fa-star-o");
-		}
+
 	}
 }
